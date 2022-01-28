3bd5c0db26565baaac0c131a1789d13b3812b10b
Juan Pablo <proyectosoftwarejp@gmail.com>
2022-01-27 20:12:31 -0500

    Pruebas con servidor

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ app.listen(3030, ()=>{
     console.log('Servidor funcionando');
 });
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/views/home.html'))
-// });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/home.html'))
+});
 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname + '/views/Formulario_registro.html'))
 // });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/views/Formulario_ingreso.html'))
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/views/Formulario_ingreso.html'))
+// });
