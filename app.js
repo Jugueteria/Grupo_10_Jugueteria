aa95429ed515b91117123e1da27122a6f6661d53
Juan Pablo <proyectosoftwarejp@gmail.com>
2022-01-29 22:50:16 -0500

    Adecuacion Formularios

--- app.js
+++ app.js
@@ -18,5 +18,5 @@ app.get('/', (req , res) => {
  });
 
 // app.get('/', (req , res) => {
-//     res.sendFile(path.join(__dirname + '/views/Formulario_ingreso.html'))
-// });
+//      res.sendFile(path.join(__dirname + '/views/Formulario_ingreso.html'))
+//  });
