be1d0fbbbbea2d79f44504075b9bbb60dc936597
ysika <ysika87@gmail.com>
2022-02-02 00:59:49 -0300

    nueva modificacion

--- app.js
+++ app.js
@@ -8,25 +8,24 @@ app.listen(3030, ()=>{
     console.log('Servidor funcionando');
 });
 
-app.get('/', (req , res) => {
-    res.sendFile(path.join(__dirname + '/views/index.html'))
-});
+// app.get('/', (req , res) => {
+//     res.sendFile(path.join(__dirname + '/views/index.html'))
+// });
 
-app.get('/', (req , res) => {
-    res.sendFile(path.join(__dirname + '/views/login.html'))
-});
+// app.get('/', (req , res) => {
+//     res.sendFile(path.join(__dirname + '/views/login.html'))
+// });
 
-app.get('/', (req , res) => {
-    res.sendFile(path.join(__dirname + '/productCart.html'))
-});
-
+// app.get('/', (req , res) => {
+//     res.sendFile(path.join(__dirname + '/productCart.html'))
+// });
 
 app.get('/', (req , res) => {
     res.sendFile(path.join(__dirname + '/views/productDetail.html'))
  });
 
 
-app.get('/', (req , res) => {
-    res.sendFile(path.join(__dirname + '/views/register.html'))
- });
+// app.get('/', (req , res) => {
+//     res.sendFile(path.join(__dirname + '/views/register.html'))
+// });
 
