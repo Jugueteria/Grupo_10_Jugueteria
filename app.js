a971d665348d82a016fd8320046a631615725034
ysika <ysika87@gmail.com>
2022-02-02 00:12:42 -0300

    modificaciones

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ app.get('/', (req , res) => {
     res.sendFile(path.join(__dirname + '/views/Formulario_registro.html'))
  });
 
+ //Ruta Detalle de Producto
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
+})
+
 // app.get('/', (req , res) => {
 //      res.sendFile(path.join(__dirname + '/views/Formulario_ingreso.html'))
 //  });
