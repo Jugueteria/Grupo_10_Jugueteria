67c05627b8ac0ac7f6dec120ce22ee117d086d78
Jhonny Valdivia <jnvald@gmail.com>
2022-02-02 00:16:39 -0300

    app rutas

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/', (req , res) => {
 });
 
 
-app.get('/', (req , res) => {
+app.get('/register', (req , res) => {
     res.sendFile(path.join(__dirname + '/views/register.html'))
  });
 
