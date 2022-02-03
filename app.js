61310b33996358afbe70b0c4101a74246107b542
Juan Pablo <proyectosoftwarejp@gmail.com>
2022-02-03 17:32:34 -0500

    Rutas de acceso

--- app.js
+++ app.js
@@ -8,25 +8,25 @@ app.listen(3030, ()=>{
     console.log('Servidor funcionando');
 });
 
-// app.get('/', (req , res) => {
-//     res.sendFile(path.join(__dirname + '/views/index.html'))
-// });
-
-
-// app.get('/', (req , res) => {
-//     res.sendFile(path.join(__dirname + '/views/login.html'))
-//  });
-
 app.get('/', (req , res) => {
+    res.sendFile(path.join(__dirname + '/views/index.html'))
+});
+
+
+app.get('/login', (req , res) => {
+    res.sendFile(path.join(__dirname + '/views/login.html'))
+ });
+
+app.get('/productCart', (req , res) => {
      res.sendFile(path.join(__dirname + '/views/productCart.html'))
  });
 
-//  app.get('/', (req , res) => {
-//     res.sendFile(path.join(__dirname + '/views/productDetail.html'))
-// });
+ app.get('/productDetail', (req , res) => {
+    res.sendFile(path.join(__dirname + '/views/productDetail.html'))
+});
 
-// app.get('/', (req , res) => {
-//     res.sendFile(path.join(__dirname + '/views/register.html'))
-// });
+app.get('/register', (req , res) => {
+    res.sendFile(path.join(__dirname + '/views/register.html'))
+});
 
 
