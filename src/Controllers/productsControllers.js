0f03fdfc7a98288cdf0ddc4db2acfdd3aa5e0019
mariaromo <mromo@rses.com.ar>
2022-05-06 15:10:10 -0300

    Crud Products

--- src/Controllers/productsControllers.js
+++ src/Controllers/productsControllers.js
@@ -1,92 +1,75 @@
-const path = require('path');
-const db = require('../database/models');
-const sequelize = db.sequelize;
-const { Op } = require("sequelize");
-const moment = require('moment');
 const app = require("../app");
 const fs = require('fs');
 const path = require('path');
+const db = require('../database/models');
+const productsFilePath = path.join(dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-const Products=db.Products;
-const Users=db.Users;
-const Brands=db.Brands;
-const carts=db.Carts;
-const Category=db.Category;
+const Products = db.Products;
+
 
 const productsControllers = {
 
-   
+
 'productCart': (req, res)=> {
-        res.render('products/productCart');
+    db.Products.findAll((req.params.product)
+    )
+    .then(products=> {
+        res.render('products.ejs',{product})
+
+    }),
+
+'productDetail': (req, res) =>{
+      db.Products.findByPk((req.params.id)
+      .then(product =>{
+          res.render('productDetail.ejs',{product});
+      })
+
+}};
+
+create: function (req, res) {
+    Products.create(
+        {
+          title: req.body.title,
+          description:req.body.description,
+          image: re.body.description
+        }
+    )
+    .then(()=> {
+        return res.redirect ('/products')})
+    .catch(error => res.send(error))
+
+  },
+
+    edit: function(req, res) {
+        let ProductId = req.params.id;
+        let productToEdit = products.findAll();
+        res.render('products/productsEdit', {productToEdit})
     },
 
+    update: (req, res)=>{
+        let Productid = req.params.id;
+        Products.update(
+            {
+        title: req.body.title,
+        description:req.body.description,
+        image: re.body.image
+            },
+        {
+        where: {id: productId}
+		}
+		.then(()=> {
+        return res.redirect('/Products')
+        .catch(error => res.send(error))
+    },
 
-'productDetail': function(req, res) {
-      let id = req.params.id
-        let product = products.find(product => product.id == id)
-        res.render('products/productDetail', {
-            product
-           
-        })
-      },
+    delete: (req, res)=> {
+        let productId = req.params.id;
+        Products.findByPk(productId)
+        .then (Product => {
+            return res.render(path.resolve(_(dirname, '..', 'views',  'productsEdit'), {Product})})
+            .catch(error => res.send(error))
+        }
 
-'formCreate': function(req, res) {
-    res.render('products/formCreate');
-  },
-
-'products': function(req, res) {
-    res.render('products/products', {products});
-  },
-
-'lista': function(req, res) {
-		let newProduct = {
-			id: products[products.length - 1].id + 1,
-			...req.body,
-			imagen: 'default.png',
-		};
-		products.push(newProduct)
-		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ' '));
-		res.redirect('/products');
-	},
-
-  'edit': function(req, res) {
-		let id = req.params.id
-		let productToEdit = products.find(product => product.id == id)
-		res.render('products/productsEdit', {productToEdit})
-	},
-
-	'update': function(req, res){
-		let id = req.params.id;
-		let productToEdit = products.find(product => product.id == id)
-
-		productToEdit = {
-			id: productToEdit.id,
-			...req.body,
-			imagen: 'default.png',
-		};
-		
-		let newProducts = products.map(product => {
-			if (product.id == productToEdit.id) {
-				return product = {...productToEdit};
-			}
-			return product;
-		})
-
-		fs.writeFileSync(productsFilePath, JSON.stringify(newProducts, null, ' '));
-		res.redirect('/');
-	},
-
-	eliminar : (req, res) => {
-        let id = req.params.id;
-        let finalProducts = products.filter(product => product.id != id);
-        fs.writeFileSync(productsFilePath, JSON.stringify(finalProducts, null, ' '));
-        res.redirect('/');
-      }
-
-
-
-
-
-  }
-  
+};
   module.exports = productsControllers;
