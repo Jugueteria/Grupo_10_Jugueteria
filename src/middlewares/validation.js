5de0e404fec1f1a453963afdbaec45772b18bb6e
Jhonny Valdivia <jnvald@gmail.com>
2022-05-20 21:41:42 -0400

    Cambio de BD a MySql - WEB 2.0

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -1,66 +1,60 @@
-const{check, body }=require("express-validator");
+const { check, body } = require("express-validator");
 const fs = require('fs');
 const path = require('path');
+const db = require("../database/models");
 
 
+module.exports = {
 
-function findAll(){
-const users= JSON.parse(fs.readFileSync(path.join(__dirname, '../data/users.json')));
-return users;
+    login: [
+        check("email")
+            .notEmpty()
+            .withMessage("Falta ingresar Email")
+            .bail()
+            .isEmail()
+            .withMessage("Formato de correo incorrecto"),
 
-}
+        check("password")
+            .notEmpty()
+            .withMessage("Falta ingresar Contraseña")
 
+    ],
+    register: [
 
-module.exports={
+        check("first_name")
+            .notEmpty()
+            .withMessage("Falta ingresar Nombre"),
 
-login:[
-    check("email")
-    .notEmpty()
-    .withMessage("Falta ingresar Email")
-    .bail()
-    .isEmail()
-    .withMessage("Formato de correo incorrecto"),
+        check("last_name")
+            .notEmpty()
+            .withMessage("Falta ingresar Apellido"),
 
-    check("password")
-    .notEmpty()
-    .withMessage("Falta ingresar Contraseña")
+        check("email")
+            .notEmpty()
+            .withMessage("Falta ingresar Email")
+            .bail()
+            .isEmail()
+            .withMessage("Formato de correo incorrecto")
+            .bail()
+            .custom(function (value) {
+               return db.user.findOne({
 
-],
-register:[
-   
-    check("first_name")
-    .notEmpty()
-    .withMessage("Falta ingresar Nombre"),
+                    where: {
+                        email: value
+                    }
+                }).then((user)=>{
+                    if (user) {
+                        return Promise.reject("Email ya registrado")
+                    }
+                })
 
-    check("last_name")
-    .notEmpty()
-    .withMessage("Falta ingresar Apellido"),
+            }),
 
-    check("email")
-    .notEmpty()
-    .withMessage("Falta ingresar Email")
-    .bail()
-    .isEmail()
-    .withMessage("Formato de correo incorrecto")
-    .bail()
-    .custom(function(value){
-      let users=findAll()
-      let userFound =users.find(function(user){
+        check("password")
+            .notEmpty()
+            .withMessage("Falta ingresar Contraseña")
 
-            return user.email==value
-        })
-
-        if(userFound){
-           throw new Error("Email ya registrado")
-        }
-        return true;
-    }),
-
-    check("password")
-    .notEmpty()
-    .withMessage("Falta ingresar Contraseña")
-    
-]
+    ]
 
 
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -9,54 +9,55 @@ const accesos=require("../middlewares/accesos");
 const sinLogin=require("../middlewares/sinLogin");
 
 
+
 const storage = multer.diskStorage({
-    destination: (req, file ,cb) => {
-       cb(null, path.join(__dirname, '../../public/images/Users') )
-    },
-    
-    filename:(req, file, cb) => {
-    
-       const newFilename='perfil-'+ Date.now() + path.extname(file.originalname) ;
-       cb(null, newFilename);
-    }
-    
-    });
-    
-    const upload = multer({storage});
-    
-    //Ingreso Login
-    router.get('/login',accesos, usersControllers.login);
-    router.post('/login',validator.login, usersControllers.ingreso); 
-    
-    //cerrar sesion
-    router.post('/logout', usersControllers.logout); 
-    //Formulario de productos
-    router.get('/form_admin', usersControllers.form_admin);
-    
-    //Lista de usuarios
-    router.get('/',sinLogin, usersControllers.users);
-    
-    //Creación de usuarios
-       router.get('/register',accesos, usersControllers.register);
-       router.post('/register', upload.single('imagenUsuario'),validator.register, usersControllers.create); 
-    
-       // Detalle usuario
-    router.get('/detail/:id',sinLogin,  usersControllers.userDetail);
-    
-    //Editar un usuario
-    
-    router.get('/edit/:id',sinLogin, usersControllers.edit); 
-    router.patch('/edit/:id',upload.single('imagenUsuario'),validator.register, usersControllers.update);
-    
-    //Borrar un usuario
-    
-    router.delete('/delete/:id', usersControllers.eliminar); 
-    
-    
-    //ver perfil de ingreso
-    
-    router.get('/profile',sinLogin, usersControllers.profile); 
-           
-           
-    
-    module.exports = router;
+destination: (req, file ,cb) => {
+   cb(null, path.join(__dirname, '../../public/images/Users') )
+},
+
+filename:(req, file, cb) => {
+
+   const newFilename='perfil-'+ Date.now() + path.extname(file.originalname) ;
+   cb(null, newFilename);
+}
+
+});
+
+const upload = multer({storage});
+
+//Ingreso Login
+router.get('/login',accesos, usersControllers.login);
+router.post('/login',validator.login, usersControllers.ingreso); 
+
+//cerrar sesion
+router.post('/logout', usersControllers.logout); 
+//Formulario de productos
+router.get('/form_admin', usersControllers.form_admin);
+
+//Lista de usuarios
+router.get('/',sinLogin, usersControllers.users);
+
+//Creación de usuarios
+   router.get('/register',accesos, usersControllers.register);
+   router.post('/register', upload.single('imagenUsuario'),validator.register, usersControllers.create); 
+
+   // Detalle usuario
+router.get('/detail/:id',sinLogin,  usersControllers.userDetail);
+
+//Editar un usuario
+
+router.get('/edit/:id',sinLogin, usersControllers.edit); 
+router.patch('/edit/:id',upload.single('imagenUsuario'),validator.register, usersControllers.update);
+
+//Borrar un usuario
+
+router.delete('/delete/:id', usersControllers.eliminar); 
+
+
+//ver perfil de ingreso
+
+router.get('/profile',sinLogin, usersControllers.profile); 
+       
+       
+
+module.exports = router;
