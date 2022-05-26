d8e20c91087a764cac1fbdbaa3cc2aa006288f99
Jhonny Valdivia <jnvald@gmail.com>
2022-05-26 13:53:16 -0400

    actualizacion multer, sweetAlert web 2.0

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -8,15 +8,15 @@ module.exports={
     },
     "test": {
       "username": "root",
-      "password": null,
-      "database": "database_test",
+      "password": "123456789",
+      "database": "Proyecto",
       "host": "127.0.0.1",
       "dialect": "mysql"
     },
     "production": {
       "username": "root",
-      "password": null,
-      "database": "database_production",
+      "password": "123456789",
+      "database": "Proyecto",
       "host": "127.0.0.1",
       "dialect": "mysql"
     }
